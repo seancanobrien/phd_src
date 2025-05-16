fe5f99f7709657f6a8c159947ee18950524e9825
Sean O'Brien <seancanobrien@hotmail.co.uk>
2025-05-16 15:44:10 +0100

    emailed jim and rachael report

--- mathematica_code/hyperbolic_coxeter_arrangements/functions/global_definitions.m
+++ mathematica_code/hyperbolic_coxeter_arrangements/functions/global_definitions.m
@@ -17,8 +17,15 @@ axes = Graphics3D[
     {Thick, Blue, Arrow[{{0, 0, -1}, {0, 0, 1}}]}
     }
    ];
+axes2DSmall = Graphics3D[
+   {
+   {Red, Arrowheads[0.01], Arrow[{{-2, 0, 0}, {2, 0, 0}}]},
+    {Green, Arrowheads[0.01], Arrow[{{0, -2, 0}, {0, 2, 0}}]}
+    }
+   ];
 bigAxes = Graphics3D[
-   {{Thick, Red, Arrow[{{-2, 0, 0}, {2, 0, 0}}]},
+   {
+   {Thick, Red, Arrow[{{-2, 0, 0}, {2, 0, 0}}]},
     {Thick, Green, Arrow[{{0, -2, 0}, {0, 2, 0}}]},
     {Thick, Blue, Arrow[{{0, 0, -2}, {0, 0, 2}}]}
     }
